46536df926ea68eff43fcadc603b0226599461d8
Eric Nguyen <itsericnguyen.ca@gmail.com>
2023-09-01 13:24:22 -0700

    Test new verifyToken function

--- jwt-project/middleware/auth.js
+++ jwt-project/middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const config = process.env;
-
+/*
 const verifyToken = (req, res, next) => {
   const token =
     req.body.token || req.query.token || req.headers["x-access-token"];
@@ -17,5 +17,24 @@ const verifyToken = (req, res, next) => {
   }
   return next();
 };
+*/
+app.get("/user/verifyToken", (req, res) => {
+let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+let jwtSecretKey = process.env.JWT_SECRET_KEY;
 
+try {
+  const token = req.header(tokenHeaderKey);
+
+  const verified = jwt.verify(token, jwtSecretKey);
+  if(verified){
+      return res.send("Successfully Verified");
+  }else{
+      // Access Denied
+      return res.status(401).send(error);
+  }
+} catch (error) {
+  // Access Denied
+  return res.status(401).send(error);
+}
+})
 module.exports = verifyToken;
