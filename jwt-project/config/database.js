65857c90527db77759e09d193d935e85e9120cd9
MadKid1000 <adelarosa1000@gmail.com>
2023-09-06 20:32:44 -0700

    updated database.js

--- jwt-project/config/database.js
+++ jwt-project/config/database.js
@@ -32,8 +32,4 @@ var con = mysql.createConnection({
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("Result: " + result);
-  });
 });
