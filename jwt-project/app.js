408aa5b2a235663e40f59199f2eff5c85e07f461
southernman <alexweber7373@gmail.com>
2023-09-15 11:46:25 -0700

    readded it for real this time LOL

--- jwt-project/app.js
+++ jwt-project/app.js
@@ -118,7 +118,34 @@ app.post("/login", async (req, res) => {
   // Our regi     ster logic ends here
 });
 
+app.post("/register", async (req,res) => {
+  // receive user information
+  const user = req.body;
+  //encrypt password
+  const encryptedPassword = await bcrypt.hash(user.password_hash, 10)  
+  // unravel JSON object
+  const user_data = [user.username, user.email, user.firstname, user.lastname, encryptedPassword, user.age]
+  // insert statement
+  const query = "insert into fitforge.users (username, email, firstname, lastname, password_hash, age) VALUES (?, ?, ?, ?, ?, ?)";
 
+  // db connection and statement execution
+  pool.mysqlConnection.query(query, user_data, (error, results) => {
+    // if query does not work, handle error here
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Server Error" });
+    }
+    // else, return a successful run
+    else {
+      return res.status(200).json({success: true}); // success
+    }
+  });
+
+  // Guys, please send back proper responses for ALL the gets and post requests, there is no way for frontend to verify
+  // unless you guys send responses back
+    //no
+
+});
 
 // tentative db connection logic
 let pool = mysql.createPool({
