778f3b15fa3956325e7cd02b359889d800c05867
MadKid1000 <adelarosa1000@gmail.com>
2023-09-20 20:52:44 -0700

    created workoutLog function

--- jwt-project/workout.js
+++ jwt-project/workout.js
@@ -116,3 +116,46 @@ app.post("/generateWorkout", async (req,res) => {
     }
 })
 
+//will get username, use that to create a sql query that grabs all workouts from the past
+//return an array of the past workouts and their lengths
+app.post("/workoutLog", async (req,res) => {
+    //db logic
+    var con = mysql.createConnection({
+        host: "fitforge.c6jigttrktuk.us-west-1.rds.amazonaws.com",
+        user: "fitforge",
+        password: "fitforge",
+        database: "fitforge"
+    });
+    const user = req.body;
+    const query = "SELECT * FROM fitforge.users WHERE username = ?"
+    con.connect(function(err){
+        if (err) throw err
+        con.query(query, user.username, function (err, result, fields) {
+            if (err) throw err;
+            if (result.length > 0) {
+                //username exists, create a 2nd and poissbly a 3rd query here to grab the data from previous workouts
+                //const query = ""
+                //temporary data
+                pastWorkouts = [
+                  ["Dumbbell Side Lunge", "Sit-Ups", "Dumbbell Split Squat", "Crunch"],
+                  ["Zottman Bicep Curl", "JM Dumbbell Bench Press", "Heel Taps", "Reverse Dumbbell Curl", "Overhead Dumbbell Extensions",
+                  "Plank Hold", "Dumbbell Wall BicepsCurl", "Dumbbell Skull Crushers"],
+                  ["Dumbbell Side Lunge", "Sit-Ups", "Dumbbell Split Squat", "Crunch"],
+                  ["Zottman Bicep Curl", "JM Dumbbell Bench Press", "Heel Taps", "Reverse Dumbbell Curl", "Overhead Dumbbell Extensions",
+                  "Plank Hold", "Dumbbell Wall BicepsCurl", "Dumbbell Skull Crushers"],
+                  ["Dumbbell Side Lunge", "Sit-Ups", "Dumbbell Split Squat", "Crunch"],
+                  ["Zottman Bicep Curl", "JM Dumbbell Bench Press", "Heel Taps", "Reverse Dumbbell Curl", "Overhead Dumbbell Extensions",
+                  "Plank Hold", "Dumbbell Wall BicepsCurl", "Dumbbell Skull Crushers"]
+              ]
+                pastDates = ["9/1/23", "9/5/23", "9/10/23", "9/14/23", "9/19/23", "9/20/23"]
+                data = [pastWorkouts, pastDates]
+                res.status(200).json(data)
+      
+              } else {
+                res.status(400).json("Invalid Username"); 
+              }
+          });
+      });
+    
+
+})
