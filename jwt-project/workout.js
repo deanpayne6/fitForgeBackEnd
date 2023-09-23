5bb0e2ffa25198c4c1e95dce279824afa5afcbe9
MadKid1000 <61327567+MadKid1000@users.noreply.github.com>
2023-09-22 18:18:09 -0700

    added sql queries to generate
    still need to fix a lot of if else statements

--- jwt-project/workout.js
+++ jwt-project/workout.js
@@ -16,45 +16,21 @@ server.listen(port, () => {
 
 //http://localhost:3306/generateWorkout
 app.post("/generateWorkout", async (req,res) => {
-    //Arrays of workouts for testing generate workout
-    //Once Database is set up, the sql queries will go here, will grab the list from each muscle group and put them into a list like shown below
-    testBicep = ["Dumbbell Curl", "Reverse Dumbbell Curl", "Dumbbell Hammer Curl", "Zottman Bicep Curl", "Concentration Bicep Curl", 
-    "Dumbbell Incline Biceps Curl", "Dumbbell Wall BicepsCurl", "Step Back BicepsCurl", "Dumbbell Pinwheel BicepsCurl", 
-    "Offset BicepsCurl", "Biceps 21s"]
-    testTricep = ["Neutral Grip Dumbbell Bench Press", "JM Dumbbell Bench Press", "Dumbbell Skull Crushers", "Dumbbell Tate Presses", 
-    "Overhead Dumbbell Extensions", "Dumbbell Kickbacks", "Dumbbell-Loaded Triceps Dips"]
-    testChest = ["Becnh Press", "incline Bench Press", "Decline Bench Press", "Chest Fly", "Dumbbell Reverse Chest Press"]
-    testBack = ["Dumbbell Row", "Incline Row", "Elevated Plank Row", "Dumbbell Pullover", "Incline Pause Row", "Bent Over Row",
-    "Upright Row", "Farmers Carry"]
-    testShoulder = ["Dumbbell shoulder press", "Dumbbell front raise", "Dumbbell side lateral raise", "Dumbbell bent-over raise", 
-        "Dumbbell upright row", "Dumbbell shoulder shrugs", "One arm dumbbell swing", "Spellcaster", "Seesaw Press"]
-    testAbs = ["Sit-Ups", "Crunch", "Heel Taps", "Plank Hold", "Leg Raises", "Bicycle Crunch", "Pike Crunch", "Reverse Crunch", "Hollow Holds"]
-    testLegs = ["Dumbbell Split Squat", "Dumbbell Romanian Deadlift", "Dumbbell Single Leg RDL", "Dumbbell Goblet Squat", 
-    "Dumbbell Side Lunge", "Dumbbell Reverse Lunge", "Dumbbell Front Squat", "Dumbbell Elevated Split Squat", 
-    "Dumbbell Forward Lunge", "Weighted Step Up", "Dumbbell Jump Squat"]
-
-    //allWorkouts and muscleGroup array will stay, the rest will not be needed once front end is done
-    allWorkouts = [testBicep, testTricep, testChest, testBack, testShoulder, testAbs, testLegs]
-    muscleGroup = [" Bicep", " Tricep", " Chest", " Back", " Shoulder", " Abs", " Legs"]
-    lengthList = ["Short", "Medium", "Long"]
-    console.log('\n1. Bicep\n2. Tricep\n3. Chest\n4. Back\n5. Shoulder\n6. Abs\n7. Legs')
-
-    //Actual logic start
     const {workoutInput, workoutLength} = req.body;
     muscleArray = []
-    chosenGroups = []
+
+    var con = mysql.createConnection({
+        host: "fitforge.c6jigttrktuk.us-west-1.rds.amazonaws.com",
+        user: "fitforge",
+        password: "fitforge",
+        database: "fitforge"
+    });
+    
     workoutList = []
     count = 0
-    for(let i = 0; i < workoutInput.length; i++){
-        temp = workoutInput[i]
-        chosenGroups.push(muscleGroup[temp-1])
-        muscleArray.push(allWorkouts[temp-1])
-    }
-    console.log("The muscle groups you chose are:" + chosenGroups)
-
     //Start of Logic for Generate Workout, everything above will change, everything below should stay the same
     //Short Workout Length, 1 Workout Chosen
-    if((workoutLength == 1) && (muscleArray.length == 1)){
+    if((workoutLength == "short") && (muscleArray.length == 1)){
         for(let i = 0; i < 3; i++){
             if(count == muscleArray.length)
                 count = 0
@@ -66,9 +42,9 @@ app.post("/generateWorkout", async (req,res) => {
         }
     }
 
-    //Short Workout Length, 2-3 Workouts Chosen
+    //Short Workout Length, 2 Workouts Chosen
     //Medium Workout Length, 1 Workout Chosen
-    else if(((workoutLength == 1) && (muscleArray.length >= 2)) || ((workoutLength == 2) && (muscleArray.length == 1))){
+    else if(((workoutLength == "short") && (muscleArray.length == 2)) || ((workoutLength == "medium") && (muscleArray.length == 1))){
         for(let i = 0; i < 4; i++){
             if(count == muscleArray.length)
                 count = 0
@@ -82,7 +58,7 @@ app.post("/generateWorkout", async (req,res) => {
 
     //Medium Workout Length, 2-3 Workouts Chosen
     //Long Workout Length, 1 Workout Chosen
-    else if(((workoutLength == 2) && (muscleArray.length >= 2)) || ((workoutLength == 3) && (muscleArray.length == 1))){
+    else if(((workoutLength == "medium") && (muscleArray.length >= 2)) || ((workoutLength == "long") && (muscleArray.length == 1))){
         for(let i = 0; i < 6; i++){
             if(count == muscleArray.length)
                 count = 0
@@ -94,27 +70,74 @@ app.post("/generateWorkout", async (req,res) => {
         }
     }
 
-    //Long Workout Length, 2-3 Workouts Chosen
-    else if((workoutLength == 3) && (muscleArray.length >= 2)){
-        for(let i = 0; i < 8; i++){
-            if(count == muscleArray.length)
-                count = 0
-            singleGroup = muscleArray[count]
-            randomWorkout = Math.floor(Math.random() * singleGroup.length)
-            workoutList.push(singleGroup[randomWorkout])
-            muscleArray[count].splice(randomWorkout, 1)
-            count++
-        }
+    //Long Workout Length, 2 Workouts Chosen
+    else if((workoutLength == "long") && (workoutInput.length == 2)){
+        tempArray = []
+        const muscleGroups = [workoutInput[0], workoutInput[1]]
+        const query = "SELECT name, muscle_group FROM fitforge.exercises WHERE muscle_group = ? + ?"
+        con.connect(function(err){
+            if (err) throw err;
+            con.query(query, muscleGroups, function (err, result){
+                if (err) throw err;
+                for(let i = 0; i < workoutInput.length; i++){
+                    for(let j = 0; j < result.length; j++){
+                        if(result[j].muscle_group == workoutInput[i]){
+                            tempArray.push(result[j].name)
+                        }
+                    }
+                    muscleArray.push(tempArray)
+                    tempArray = []
+                }
+                for(let i = 0; i < 8; i++){
+                    if(count == workoutInput.length)
+                        count = 0
+                    singleGroup = muscleArray[count]
+                    randomWorkout = Math.floor(Math.random() * singleGroup.length)
+                    workoutList.push(singleGroup[randomWorkout])
+                    muscleArray[count].splice(randomWorkout, 1)
+                    count++
+                }
+                res.status(200).json(workoutList)
+            })
+        })
     }
 
-    if(workoutList.length > 0){
-        console.log("Here is your workout for the day: ")
-        res.status(200).json(workoutList)
-        console.log("Thank You For Using FitForge!")
+    //Long Workout Length, 3 Workouts Chosen
+    else if((workoutLength == "long") && (workoutInput.length == 3)){
+        tempArray = []
+        const muscleGroups = [workoutInput[0], workoutInput[1], workoutInput[2]]
+        const query = "SELECT name, muscle_group FROM fitforge.exercises WHERE muscle_group = ? + ? + ?"
+        con.connect(function(err){
+            if (err) throw err;
+            con.query(query, muscleGroups, function (err, result){
+                if (err) throw err;
+                for(let i = 0; i < workoutInput.length; i++){
+                    for(let j = 0; j < result.length; j++){
+                        if(result[j].muscle_group == workoutInput[i]){
+                            tempArray.push(result[j].name)
+                        }
+                    }
+                    muscleArray.push(tempArray)
+                    tempArray = []
+                }
+                for(let i = 0; i < 8; i++){
+                    if(count == workoutInput.length)
+                        count = 0
+                    singleGroup = muscleArray[count]
+                    randomWorkout = Math.floor(Math.random() * singleGroup.length)
+                    workoutList.push(singleGroup[randomWorkout])
+                    muscleArray[count].splice(randomWorkout, 1)
+                    count++
+                }
+                res.status(200).json(workoutList)
+            })
+        })
     }
+
     else{
         res.status(400).send("Invalid Inputs")
     }
+    
 })
 
 //will get username, use that to create a sql query that grabs all workouts from the past
@@ -132,7 +155,7 @@ app.post("/workoutLog", async (req,res) => {
     const query = "SELECT * FROM fitforge.users WHERE username = ?"
     con.connect(function(err){
         if (err) throw err
-        con.query(query, user.username, function (err, result, fields) {
+        con.query(query, user.username, function (err, result) {
             if (err) throw err;
             if (result.length > 0) {
                 //username exists, create a 2nd and poissbly a 3rd query here to grab the data from previous workouts
