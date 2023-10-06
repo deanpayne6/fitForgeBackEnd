d9b154af78d102a1e05ab11ce7587dc3014321c0
alexweber7373 <alexweber7373@gmail.com>
2023-10-06 14:15:54 -0700

    mk1 user model backend

--- jwt-project/model/user.js
+++ jwt-project/model/user.js
@@ -1,11 +1,13 @@
-// var mysql = require('mysql');
 
-// const userSchema = mysql.Schema({
-//   first_name: { type: String, default: null },
-//   last_name: { type: String, default: null },
-//   email: { type: String, unique: true },
-//   password: { type: String },
-//   token: { type: String },
-// });
+class User {
+  constructor(email, username, first_name, last_name, age) {
+    this.email = email,
+    this.username = username,
+    this.first_name = firstname,
+    this.last_name = lastname,
+    this.age = age
+  }
+}
 
-// module.exports = mysql.model("user", userSchema);
+
+
