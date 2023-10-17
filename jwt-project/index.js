29725a91cb6038e1c12dbfdb072bd8c2b8d4ba0f
southernman <alexweber7373@gmail.com>
2023-10-17 15:32:54 -0700

    https functionality

--- jwt-project/index.js
+++ jwt-project/index.js
@@ -1,8 +1,9 @@
 const http = require("http");
+const https = require("https");
 const app = require("./app");
-const server = http.createServer(app);
+const fs = require("fs");
 
-const allowedOrigins = ["http://localhost:4200"]; 
+const allowedOrigins = ["https://localhost:4200"]; 
 
 // server.on("request", (req, res) => {
 //   const origin = req.headers.origin;
@@ -18,6 +19,13 @@ const allowedOrigins = ["http://localhost:4200"];
 // });
 
 const port = 3200;
+
+const options = {
+  key: fs.readFileSync("keys/privkey.pem"),
+  cert: fs.readFileSync("keys/fullchain.pem")
+}
+
+const server = https.createServer(options, app);
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
