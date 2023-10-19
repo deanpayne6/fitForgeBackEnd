09e3f7a8ec2bf72fcc2060fe1a67934b4bbc4f57
alexweber7373 <alexweber7373@gmail.com>
2023-10-18 18:58:32 -0700

    misc

---
+++ jwt-project/model/user.js
@@ -0,0 +1,13 @@
+
+class User {
+  constructor(email, username, first_name, last_name, age) {
+    this.email = email,
+    this.username = username,
+    this.first_name = firstname,
+    this.last_name = lastname,
+    this.age = age
+  }
+}
+
+
+
