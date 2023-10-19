3ee385b2801cc886b8a4a26dde7fc43075922717
alexweber7373 <alexweber7373@gmail.com>
2023-10-18 19:08:00 -0700

    got rid of commented code

--- jwt-project/app.js
+++ jwt-project/app.js
@@ -20,56 +20,7 @@ const auth = require("./middleware/auth");
 
 app.use(cors());
 
-// // Register
-//   app.post("/register", async (req,  res) => {
-//     try {
-//         // Get user input
-//         const { first_name, last_name, email, password } = req.body;
-        
-    
-//         // Validate user input
-//         if (!(email && password && first_name && last_name)) {
-//           res.status(400).send("All input is required");
-//         }
-    
-//         // check if user already exist
-//         // Validate if user exist in our database
-//         const oldUser = await User.findOne({ email });
-    
-//         if (oldUser) {
-//           return res.status(409).send("User Already Exist. Please Login");
-//         }
-    
-//         //Encrypt user password
-//         const encryptedPassword = await bcrypt.hash(password, 10);
-    
-//         // Create user in our database
-//         const user = await User.create({
-//           first_name,
-//           last_name,
-//           email: email.toLowerCase(), // sanitize: convert email to lowercase
-//           password: encryptedPassword,
-//         });
-    
-//         // Create token
-//         const token = jwt.sign(
-//           { user_id: user._id, email },
-//           "" + process.env.TOKEN_KEY,
-//           {
-//             expiresIn: "2h",
-//           }
-//         );
-//         // save user token
-//         user.token = token;
-    
-//         // return new user
-//         res.status(201).json(user);
-//       } catch (err) {
-//         console.log(err);
-//         res.status(500).send("Server Error");
-//       }
-//       // Our register logic ends here
-// });
+
 
 const pool = mysql.createPool({
   host: "fitforge.c6jigttrktuk.us-west-1.rds.amazonaws.com",
@@ -144,44 +95,7 @@ app.post("/login", async (req, res) => {
     }
   });
 
-  // compare password to hashed password for said username
 
-  // if username exists and password hash matches, generate a login token with a success code
-
-// try {
-//     // Get user input
-//     const user1 = req.body;
-
-//     // Validate user input
-//     if (!(email && password)) {
-//       res.status(400).send("All input is required");
-//     }
-//     // Validate if user exist in our database
-//     const user = await User.findOne({ email });
-
-//     if (user && (await bcrypt.compare(password, user.password))) {
-//       // Create token
-//       const token = jwt.sign(
-//         { user_id: user._id, email },
-//         "" + process.env.TOKEN_KEY,
-//         {
-//           expiresIn: "2h",
-//         }
-//       );
-
-//       // save user token
-//       user.token = token;
-
-//       // user
-//       res.status(200).json(user);
-//     }
-//     res.status(400).send("Invalid Credentials");
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("Server Error");
-//   }
-  
-  // Our register logic ends here
 });
 
 app.use((req, res, next) => {
@@ -221,71 +135,7 @@ app.post("/register", async (req,res) => {
       return res.status(201).json({success: true}); // success
     }
   });
-
- //generate random token for password recovery.
- /*
- async function generateToken() {
-   // Generate a unique value, e.g., a timestamp or a random string
-  const uniqueValue = Date.now().toString(); // You can use any unique value
-  const token = await bcrypt.hash(uniqueValue, 10); // Hash the unique value with bcrypt
-
-  return token;
-}
-*/
-//request password
-/*
-app.post('/forgot-password', async (req, res) => {
-  try {
-    const { email } = req.body;
-
-    // Find the user with the provided email address
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    // Generate a secure token for password reset
-    const token = generateToken();
-
-    // Save the token and its expiration time in the user document
-    user.resetPasswordToken = token;
-    user.resetPasswordExpires = Date.now() + 3600000; // Token expires in 1 hour
-
-    await user.save();
-
-    // Send a password reset email to the user
-    const resetLink = `fitforge.c6jigttrktuk.us-west-1.rds.amazonaws.com/reset-password?token=${token}`; // placeholder for now
-    const mailOptions = {
-      from: 'fitForgeSupport@gmail.com', // placeholder for now
-      to: email,
-      subject: 'Password Reset Request',
-      text: `You are receiving this email because you (or someone else) have requested a password reset. Please click on the following link to reset your password: ${resetLink}`,
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Failed to send reset email' });
-      }
-
-      console.log(`Password reset email sent to: ${email}`);
-      res.status(200).json({ message: 'Password reset email sent' });
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
-  }
 });
-*/
-  // Guys, please send back proper responses for ALL the gets and post requests, there is no way for frontend to verify
-  // unless you guys send responses back
-    //no
-
-});
-
-// tentative db connection logic
-
 
 app.get("/checkEmailAvailability", (req, res) => {
   const { email } = req.query;
