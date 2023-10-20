ef9b1f73824b18ab50f92083b31b28a8d408ea53
southernman <alexweber7373@gmail.com>
2023-10-20 15:52:48 -0700

    questionnaire mk1 backend

---
+++ jwt-project/models/questionnaire.js
@@ -0,0 +1,17 @@
+
+class questionnaire {
+  constructor(activity, experience, gym, goal, intensity, frequency, gender, weight, height, emailaddress) {
+    this.activity = activity,
+    this.experience = experience,
+    this.gym = gym,
+    this.goal = goal,
+    this.intensity = intensity,
+    this.frequency = frequency,
+    this.gender = gender,
+    this.weight = weight,
+    this.height = height,
+    this.emailaddress = emailaddress
+  }
+}
+
+module.exports = questionnaire;
