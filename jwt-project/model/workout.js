24ec25daa15e25d8721a65de8ef2449d0e0c69e8
Eric Nguyen <itsericnguyen.ca@gmail.com>
2023-10-21 20:20:26 -0700

    Changes to workout.js

--- jwt-project/model/workout.js
+++ jwt-project/model/workout.js
@@ -1,14 +1,12 @@
-var mysql = require('mysql');
 
-const workoutSchema = mysql.Schema({
-    workout: {type: String, default:null},
-    muscle_group: { type: String, default: null },
-    sets: { type: int, default: null },
-    reps: { type: int,  default: null},
-    rest: { type: int, default: null},
-    target: {type: int, default: null}
-
-});
-
- module.exports = mysql.model("workout", workoutSchema);
+class Workout {
+    constructor(workout, muscle_group, sets, reps, rest, target) {
+      this.workout = workout,
+      this.muscle_group = muscle_group,
+      this.sets = sets,
+      this.reps = reps,
+      this.rest = rest,
+      this.target == target
+    }
+  }
 
